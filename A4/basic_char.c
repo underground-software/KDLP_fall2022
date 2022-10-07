454e103e6e8f50710ed6152adbd8ec5a0e41a49e
Charles Mirabile <cmirabil@redhat.com>
2022-10-06 22:54:28 -0400

    A4: Add code to make file appear in /dev/
    This code builds on the code written in class on Wednesday
    and adds a bit more that I didn't have time to cover.
    
    It calls a few more kernel functions so that a special
    character device file will be created for the driver in /dev/
    
    Signed-off-by: Charles Mirabile <cmirabil@redhat.com>

--- A4/basic_char.c
+++ A4/basic_char.c
@@ -8,6 +8,8 @@ MODULE_LICENSE("GPL");
 
 static dev_t basic_char_device_no;
 static struct cdev *basic_char_cdev;
+static struct class *basic_char_class;
+static struct device *basic_char_device;
 
 static int basic_char_open(struct inode *inode, struct file *file)
 {
@@ -42,6 +44,13 @@ static const struct file_operations basic_char_fops = {
 	.write = basic_char_write,
 };
 
+static char *basic_char_node(struct device *dev, umode_t *mode)
+{
+	if(mode)
+		*mode = 0666;
+	return NULL;
+}
+
 static int basic_char_init(void)
 {
 	int ret;
@@ -68,9 +77,25 @@ static int basic_char_init(void)
 		goto fail_add;
 	}
 
-	printk(KERN_INFO "Basic Char Device: got major %i, minor %i\n", MAJOR(basic_char_device_no), MINOR(basic_char_device_no));
+	basic_char_class = class_create(THIS_MODULE, "basic_char");
+	if (IS_ERR(basic_char_class)) {
+		ret = PTR_ERR(basic_char_class);
+		printk(KERN_ERR "Basic Char Device: unable to create device class %i\n", ret);
+		goto fail_add;
+	}
+
+	basic_char_class->devnode = basic_char_node;
+	basic_char_device = device_create(basic_char_class, NULL, basic_char_device_no, NULL, "basic_char");
+	if(IS_ERR(basic_char_device)) {
+		ret = PTR_ERR(basic_char_device);
+		printk(KERN_ERR "Basic Char Device: unable to create device %i\n", ret);
+		goto fail_class;
+	}
+
 	return 0;
 
+fail_class:
+	class_destroy(basic_char_class);
 fail_add:
 	cdev_del(basic_char_cdev);
 fail_cdev:
@@ -82,6 +107,8 @@ fail_region:
 static void basic_char_exit(void)
 {
 	printk(KERN_INFO "Basic Char Device: exit\n");
+	device_destroy(basic_char_class, basic_char_device_no);
+	class_destroy(basic_char_class);
 	cdev_del(basic_char_cdev);
 	unregister_chrdev_region(basic_char_device_no, 1);
 }
